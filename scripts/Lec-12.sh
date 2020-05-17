80a010aa5440f2fbc13ed73a6fdf512449b4894f
Rashedul Islam <rashedul.gen@gmail.com>
2020-05-17 14:45:22 -0700

    updated readme

---
+++ scripts/Lec-12.sh
@@ -0,0 +1,21 @@
+#for loop
+cd ~/Documents/Linux/data/bed/
+
+#example
+for file in *bed;
+    do echo $file;
+done
+
+#
+for file in *bed;
+    do echo $file;
+    less $file | tail -2;
+    echo "printing two lines at the end";
+    less $file | wc -l;
+done
+
+#one line
+for file in *bed; do echo $file; done
+
+#
+for file in *bed; do echo $file; less $file | tail -2; echo "printing two lines at the end"; done
