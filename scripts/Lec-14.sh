ee42284ff0dee9bd47724d09f18f825ca087aeaf
Rashedul Islam <rashedul.gen@gmail.com>
2020-05-18 10:39:53 -0700

    updated readme

--- scripts/Lec-14.sh
+++ scripts/Lec-14.sh
@@ -1,6 +1,23 @@
 #basename
+man basename
 
-for f in /Users/rashedulislam/Documents/Linux/data/bed/*bed; 
+# write a for loop
+for f in ~/Documents/Linux/data/bed/*bed; 
+    do echo $f; 
+done
+
+# use of basename
+for f in ~/Documents/Linux/data/bed/*bed; 
+    do basename $f _peaks.bed; 
+done
+
+#remove file suffix
+for f in ~/Documents/Linux/data/bed/*bed; 
+    do basename $f .bed; 
+done
+
+#remove file suffix 
+for f in ~/Documents/Linux/data/bed/*bed; 
     do basename $f _peaks.bed; 
 done
 
@@ -8,14 +25,3 @@ done
 
 
 
-
-
-
-<p align="center">
-  <b>Follow বায়োইনফরমেটিক্স স্কুল:</b><br>
-  <a href="https://www.facebook.com/%E0%A6%AC%E0%A6%BE%E0%A6%AF%E0%A6%BC%E0%A7%8B%E0%A6%87%E0%A6%A8%E0%A6%AB%E0%A6%B0%E0%A6%AE%E0%A7%87%E0%A6%9F%E0%A6%BF%E0%A6%95%E0%A7%8D%E0%A6%B8-%E0%A6%B8%E0%A7%8D%E0%A6%95%E0%A7%81%E0%A6%B2-575599666193690/">facebook page</a> |
-  <a href="https://www.facebook.com/groups/390262838074549/">facebook group</a> |
-  <a href="https://www.youtube.com/channel/UCm-8CdrvGi2SjLEOUSCztIg?view_as=subscriber">YouTube</a>
-  <br><br>
-  <img src="../files/logo.png" height="100" width="100">
-</p>
